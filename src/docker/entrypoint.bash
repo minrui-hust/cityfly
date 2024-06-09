4bf8e00df0f8e93b2d9b566e5ca25851db80dede
minrui <hustminrui@126.com>
2024-06-09 17:23:26 +0800

    support docker launch

--- src/docker/entrypoint.bash
+++ src/docker/entrypoint.bash
@@ -1,10 +1,9 @@
 #! /bin/bash
 
-launch_cmd=$1
+launch_cmd=$*
 
 source /opt/cityfly/setup.bash
 export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
 export FASTRTPS_DEFAULT_PROFILES_FILE=/opt/cityfly/share/fastrtps.xml
 
-# ros2 launch $launch_cmd
-ros2 run sensor oak_ffc_4p
+ros2 launch $launch_cmd
