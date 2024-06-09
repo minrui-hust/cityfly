0eefd61f9830ea3f672b9802ef097660846eb628
mr <mr>
2024-06-09 11:05:32 +0800

    docker file for dev

---
+++ src/docker/run_dev.bash
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+workspace=$1
+
+docker run -it \
+  --privileged \
+  --ipc host \
+  --network host \
+  --env DISPLAY=$DISPLAY \
+  --volume /dev:/dev \
+  --volume /tmp/.X11-unix:/tmp/.X11-unix \
+  --volume $workspace:/home/ubuntu/workspace \
+  cityfly:dev
